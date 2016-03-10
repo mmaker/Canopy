80e490fc6fdc7dd7884e53583580c1beaceff620
Enguerrand Decorne <decorne.en@gmail.com>
2016-03-10 18:24:41 +0000

    Change default index page

--- canopy_config.ml
+++ canopy_config.ml
@@ -7,7 +7,7 @@ type t = {
 
 let config = {
   remote_uri = "https://github.com/Engil/__blog.git";
-  index_page = "Index.md";
+  index_page = "Index";
   port = 8080;
   push_hook_path = "push";
 }
