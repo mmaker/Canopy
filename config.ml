83c3641e51326cda8088e292315d2f7a1b7ba45f
Michele Orrù <maker@tumbolandia.net>
2016-03-11 21:00:55 +0000

    Add network stack for working with xen.
    Furthermore, add packages strictly for xen and cleanup optional labeled
    arguments.

--- config.ml
+++ config.ml
@@ -1,7 +1,5 @@
 open Mirage
 
-let stack console = socket_stackv4 console [Ipaddr.V4.any]
-
 let disk = crunch "./disk"
 
 let index_k =
@@ -28,12 +26,28 @@ let main =
   let libraries = [
       "cow";
       "decompress";
-      "irmin.git";
       "irmin.mirage";
+      "irmin.git";
       "mirage-http";
       "tls.mirage";
     ] in
   let libraries = if get_mode () = `Xen then libraries else "irmin.unix" :: libraries in
+
+  let packages = [
+      "cow";
+      "decompress";
+      "irmin";
+      "mirage-http";
+      "mirage-flow";
+      "tls";
+      "mirage-types-lwt";
+      "channel";
+      "mirage-git";
+      "re";
+      "cohttp"
+    ] in
+  let packages = if get_mode () = `Xen then "mirage-xen" :: packages else "git-unix" :: packages in
+
   let keys = Key.([
                      abstract index_k;
                      abstract name_k;
@@ -44,16 +58,45 @@ let main =
   foreign
     ~libraries
     ~deps:[abstract nocrypto]
-    ~keys:keys
-    ~packages:["decompress";"irmin"; "mirage-http"; "mirage-flow"; "tls"; "cow";
-               "mirage-types-lwt"; "channel"; "git"; "mirage-git"; "git-unix"; "re"; "cohttp"]
+    ~keys
+    ~packages
     "Canopy_main.Main" (console @-> resolver @-> conduit @-> http @-> kv_ro @-> job)
+
+let net =
+  match get_mode () with
+  | `Xen -> `Direct
+  | `Unix ->
+     try match Sys.getenv "NET" with
+         | "direct" -> `Direct
+         | "socket" -> `Socket
+         | _        -> `Direct
+     with Not_found -> `Socket
+
+let ipv4_conf =
+  let i = Ipaddr.V4.of_string_exn in
+  {
+    address  = i "10.0.0.2";
+    netmask  = i "255.255.255.0";
+    gateways = [i "10.0.0.1"];
+  }
+
+let stack console =
+  match net with
+  | `Socket -> socket_stackv4 console [Ipaddr.V4.any]
+  | `Direct ->
+     try match Sys.getenv "DHCP" with
+         | "no" -> direct_stackv4_with_static_ipv4 console tap0 ipv4_conf
+         | "yes" -> direct_stackv4_with_dhcp console tap0
+         | _ -> raise Not_found
+     with Not_found -> failwith "Set DHCP to 'yes' or 'no'"
+
 
 let () =
   let sv4 = stack default_console in
   let res_dns = resolver_dns sv4 in
   let conduit = conduit_direct sv4 ~tls:true in
   let http_srv = http_server conduit in
+
   register "canopy" [
     main $ default_console $ res_dns $ conduit $ http_srv $ disk
   ]
