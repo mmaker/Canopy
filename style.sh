5e3c42f9ceaa464d33964a60afae3f94a2273d70
Gina Maini <gina@beancode.io>
2016-03-24 21:53:56 -0700

    Atomic less with flat import structure, spring cleaning, less upgrade

--- style.sh
+++ style.sh
@@ -1,3 +1,3 @@
 mkdir -p disk
-bower install
-lessc -x less/style.less disk/static/css/style.css
+bower update && bower install
+lessc less/style.less disk/static/css/style.css --source-map-map-inline --strict-imports 
