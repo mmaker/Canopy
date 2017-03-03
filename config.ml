f25fa552816cb0ed02c5e9dc1091f044753c6d2e
Michele Orrù <maker@tumbolandia.net>
2017-03-03 14:06:02 +0100

    Build fails with Decompress 0.5.0.
    Restore build to a working state by fixing max version for Decompress, and get
    @dinosaure to do a pull request. :PPP

--- config.ml
+++ config.ml
@@ -27,7 +27,7 @@ let packages = [
   package "omd" ;
   package ~min:"4.0.0" "tyxml";
   package "ptime";
-  package "decompress";
+  package ~max:"0.4.0" "decompress";
   package "irmin";
   package "irmin-mirage";
   package "mirage-http";
